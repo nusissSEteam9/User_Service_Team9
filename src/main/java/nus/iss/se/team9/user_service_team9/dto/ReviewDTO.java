91d68a771bc3fe70a8f5fae3eb9461f3ca049cef
ColdSharpTen <E1221820@n.nus.edu>
2024-10-25 15:34:23 +0800

    Update view review list action and add dto

---
+++ src/main/java/nus/iss/se/team9/user_service_team9/dto/ReviewDTO.java
@@ -0,0 +1,23 @@
+package nus.iss.se.team9.user_service_team9.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewDTO {
+    private Integer reviewId;
+    private String recipeName;
+    private Integer recipeId;
+    private Integer rating;
+    private String comment;
+
+    // Constructors
+    public ReviewDTO(Integer reviewId, String recipeName, Integer recipeId, Integer rating, String comment) {
+        this.reviewId = reviewId;
+        this.recipeName = recipeName;
+        this.recipeId = recipeId;
+        this.rating = rating;
+        this.comment = comment;
+    }
+}
